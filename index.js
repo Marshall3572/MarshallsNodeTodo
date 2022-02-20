f444712a5867bd8cb1eff56786ed1926c04de528
Marshall3572 <944797458@qq.com>
2022-02-20 18:46:52 +0800

    feat: eliminate webstorm warning

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const fs = require('fs')
-const p = require('path')
-// home 地址
-const homedir = require('os').homedir()
-// home变量（系统地址）,用户可能在环境变量中修改了默认的HOME地址，所以我们推荐用下边这种获取 home 地址的方式实现
-const home = process.env.HOME || homedir
-const dbPath = p.join(home, '.todo')
 const db = require("./db");
 
 module.exports.add = async (title) => {
